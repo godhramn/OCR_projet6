6cc53352f3b62ed1baadced20ccd2deb7669043a
godhramn <jean.degrange@gmail.com>
2022-08-19 15:48:02 +0200

    added password validator

--- controllers/user.js
+++ controllers/user.js
@@ -45,3 +45,5 @@ exports.login = (req, res, next) => {
     })
     .catch((error) => res.status(500).json({ error }));
 };
+
+
---
+++ middleware/password-validator.js
@@ -0,0 +1,21 @@
+const passwordValidator = require("password-validator");
+
+const schema = new passwordValidator();
+
+schema
+  .is()
+  .min(8)
+  .is()
+  .max(64);
+
+module.exports = (req, res, next) => {
+  if (schema.validate(req.body.password)) {
+    next();
+  } else {
+    res.status(400).json({
+      error:
+        "The password is invalid : " +
+        schema.validate(req.body.password, { list: true }),
+    });
+  }
+};
